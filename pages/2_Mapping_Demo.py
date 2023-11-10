99674d432d7fda4b05be7a49a0a522dab9f657d3
HALOUA1987 <131009546+HALOUA1987@users.noreply.github.com>
2023-11-10 20:02:07 +0000

    URL analysis (requirements modif

--- pages/2_Mapping_Demo.py
+++ pages/2_Mapping_Demo.py
@@ -1,117 +1,187 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-from urllib.error import URLError
-
-import pandas as pd
-import pydeck as pdk
-
 import streamlit as st
-from streamlit.hello.utils import show_code
+from langchain.llms import CTransformers
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.docstore.document import Document
+from langchain.document_loaders import PyPDFLoader
+from langchain.embeddings import HuggingFaceBgeEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains import RetrievalQA
+import logging
+from unstructured.partition.pdf import partition_pdf
+from pydantic import BaseModel
+from typing import Any, List
+import os
+import subprocess
+import requests
+from urllib.parse import urlparse
+import pdfkit
+import subprocess
+import shlex
 
-
-def mapping_demo():
-    @st.cache_data
-    def from_data_file(filename):
-        url = (
-            "https://raw.githubusercontent.com/streamlit/"
-            "example-data/master/hello/v1/%s" % filename
-        )
-        return pd.read_json(url)
-
+def check_and_install_libgl():
     try:
-        ALL_LAYERS = {
-            "Bike Rentals": pdk.Layer(
-                "HexagonLayer",
-                data=from_data_file("bike_rental_stats.json"),
-                get_position=["lon", "lat"],
-                radius=200,
-                elevation_scale=4,
-                elevation_range=[0, 1000],
-                extruded=True,
-            ),
-            "Bart Stop Exits": pdk.Layer(
-                "ScatterplotLayer",
-                data=from_data_file("bart_stop_stats.json"),
-                get_position=["lon", "lat"],
-                get_color=[200, 30, 0, 160],
-                get_radius="[exits]",
-                radius_scale=0.05,
-            ),
-            "Bart Stop Names": pdk.Layer(
-                "TextLayer",
-                data=from_data_file("bart_stop_stats.json"),
-                get_position=["lon", "lat"],
-                get_text="name",
-                get_color=[0, 0, 0, 200],
-                get_size=10,
-                get_alignment_baseline="'bottom'",
-            ),
-            "Outbound Flow": pdk.Layer(
-                "ArcLayer",
-                data=from_data_file("bart_path_stats.json"),
-                get_source_position=["lon", "lat"],
-                get_target_position=["lon2", "lat2"],
-                get_source_color=[200, 30, 0, 160],
-                get_target_color=[200, 30, 0, 160],
-                auto_highlight=True,
-                width_scale=0.0001,
-                get_width="outbound",
-                width_min_pixels=3,
-                width_max_pixels=30,
-            ),
-        }
-        st.sidebar.markdown("### Map Layers")
-        selected_layers = [
-            layer
-            for layer_name, layer in ALL_LAYERS.items()
-            if st.sidebar.checkbox(layer_name, True)
-        ]
-        if selected_layers:
-            st.pydeck_chart(
-                pdk.Deck(
-                    map_style=None,
-                    initial_view_state={
-                        "latitude": 37.76,
-                        "longitude": -122.4,
-                        "zoom": 11,
-                        "pitch": 50,
-                    },
-                    layers=selected_layers,
-                )
-            )
-        else:
-            st.error("Please choose at least one layer above.")
-    except URLError as e:
-        st.error(
-            """
-            **This demo requires internet access.**
-            Connection error: %s
-        """
-            % e.reason
-        )
+        result = subprocess.run(["ldconfig", "-p"], text=True, capture_output=True)
+        if "libGL.so.1" not in result.stdout:
+            print("Installing libGL.so.1...")
+            subprocess.run(["sudo", "apt-get", "update"], check=True)
+            subprocess.run(["sudo", "apt-get", "install", "-y", "libgl1-mesa-glx"], check=True)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+check_and_install_libgl()
+
+logging.basicConfig(level=logging.INFO)
+
+def load_llm(model_name):
+    llm = CTransformers(
+        model=model_name,
+        model_type="mistral",
+        max_new_tokens=1048,
+        temperature=0
+    )
+    return llm
 
 
-st.set_page_config(page_title="Mapping Demo", page_icon="🌍")
-st.markdown("# Mapping Demo")
-st.sidebar.header("Mapping Demo")
+
+class Element(BaseModel):
+    type: str
+    text: Any
+
+def file_processing(file_path: str) -> List[Element]:
+    raw_pdf_elements = partition_pdf(
+        filename=file_path,
+        extract_images_in_pdf=False,
+        infer_table_structure=True,
+        chunking_strategy="by_title",
+        max_characters=4000,
+        new_after_n_chars=3800,
+        combine_text_under_n_chars=2000,
+    )
+
+    categorized_elements = []
+    for element in raw_pdf_elements:
+        if "unstructured.documents.elements.Table" in str(type(element)):
+            categorized_elements.append(Element(type="table", text=str(element)))
+        elif "unstructured.documents.elements.CompositeElement" in str(type(element)):
+            categorized_elements.append(Element(type="text", text=str(element)))
+
+    return categorized_elements
+
+@st.cache(allow_output_mutation=True, suppress_st_warning=True)
+def llm_pipeline(file_path: str, model_name: str):
+    elements = file_processing(file_path)
+
+    # Segregate text and table elements
+    texts = [e.text for e in elements if e.type == "text"]
+    tables = [e.text for e in elements if e.type == "table"]
+
+    # Process text elements
+    documents_text = [Document(page_content=t) for t in texts]
+    embeddings_text = HuggingFaceBgeEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+    vector_store_text = FAISS.from_documents(documents_text, embeddings_text)
+
+    # Process table elements
+    documents_table = [Document(page_content=t) for t in tables]
+    embeddings_table = HuggingFaceBgeEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+    vector_store_table = FAISS.from_documents(documents_table, embeddings_table)
+
+    # Create retriever for text and table elements
+    retriever_text = vector_store_text.as_retriever()
+    retriever_table = vector_store_table.as_retriever()
+
+    # Load llm and create answer generation chain
+    llm_answer_gen = load_llm(model_name)
+    answer_generation_chain_text = RetrievalQA.from_chain_type(llm=llm_answer_gen, chain_type="stuff", retriever=retriever_text)
+    answer_generation_chain_table = RetrievalQA.from_chain_type(llm=llm_answer_gen, chain_type="stuff", retriever=retriever_table)
+
+    # You might want to combine or utilize the text and table retrievers/chains accordingly in your app
+    return answer_generation_chain_text, answer_generation_chain_table
+
+def html_to_pdf(input_html, output_pdf):
+    command = f"xvfb-run wkhtmltopdf --enable-local-file-access --quiet --no-images --disable-external-links --disable-javascript {shlex.quote(input_html)} {shlex.quote(output_pdf)}"
+    try:
+        subprocess.check_call(command, shell=True)
+        print('PDF conversion successful!')
+        return output_pdf
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred during PDF conversion: {e}")
+        return None
+
+    
+def url_to_pdf(full_url, personal_access_token):
+    # Parse the full URL to separate the domain and the path
+    parsed_url = urlparse(full_url)
+    domain = parsed_url.netloc
+    page_path = parsed_url.path
+
+    headers = {
+        "Authorization": f"Bearer {personal_access_token}",
+        "Accept": "application/json"
+    }
+
+    # Make the request to the specified URL
+    response = requests.get(f'https://{domain}{page_path}', headers=headers)
+    
+    # Check the response
+    if response.status_code == 200:
+        print('Connection successful!')
+        
+        # Save the HTML content to a file
+        with open('temp_pdf.html', 'w') as file:
+            file.write(response.text)
+        
+        # Convert the HTML file to PDF using the html_to_pdf function
+        return html_to_pdf('temp_pdf.html', 'temp_pdf.pdf')
+        
+    else:
+        print(f'Failed to connect, status code: {response.status_code}')
+        print('Response:', response.text)
+        return None
+
+def question_over_pdf_app():
+    model_selection = st.sidebar.selectbox(
+        'Select a Model:',
+        ('TheBloke/Mistral-7B-Instruct-v0.1-GGUF', 'TheBloke/zephyr-7B-alpha-GGUF', 'TheBloke/zephyr-7B-beta-GGUF')
+    )
+
+    uploaded_file = st.sidebar.file_uploader("Upload your PDF file here", type=['pdf'])
+    file_path_input = st.sidebar.text_input("...or enter a file path here")
+    confluence_url = st.sidebar.text_input("...or enter a Confluence page URL here")
+    personal_access_token = st.sidebar.text_input("Enter your Confluence Personal Access Token")
+
+    file_to_process = None
+
+    if uploaded_file:
+        file_to_process = "temp_pdf.pdf"
+        with open(file_to_process, "wb") as f:
+            f.write(uploaded_file.getvalue())
+    elif file_path_input:
+        file_to_process = file_path_input
+    elif confluence_url and personal_access_token:
+        # Call the url_to_pdf function and get the path to the processed PDF
+        file_to_process = url_to_pdf(confluence_url, personal_access_token)
+
+    if file_to_process:
+        with st.spinner("Analyzing..."):
+            answer_generation_chain_text, answer_generation_chain_table = llm_pipeline(file_to_process, model_selection)
+        st.sidebar.success("PDF Analyzed! You can now ask questions.")
+
+        question = st.sidebar.text_input("Posez votre question ici")
+
+        if st.sidebar.button("Ask"):
+            with st.spinner("Fetching answer..."):
+                response_text = answer_generation_chain_text.run(question)
+                response_table = answer_generation_chain_table.run(question)
+                st.write(f"Text Response: {response_text}")
+                st.write(f"Table Response: {response_table}")
+
+st.set_page_config(page_title="Question over PDF using HF", page_icon="📖")
+st.markdown("# Question over text and table using HF")
+st.sidebar.header("Question over text and table using HF")
 st.write(
-    """This demo shows how to use
-[`st.pydeck_chart`](https://docs.streamlit.io/library/api-reference/charts/st.pydeck_chart)
-to display geospatial data."""
+    """This app allows you to upload a PDF, select a model, and ask questions based on the content of the text and table."""
 )
 
-mapping_demo()
+question_over_pdf_app()
 
-show_code(mapping_demo)
+
